4c92c9e116b246173cc932e62e5dbdecd50984b5
Valentina <raton13@rambler.ru>
2015-07-17 20:34:07 +0300

    Initial commit

---
+++ src/linkedlist/LinkedList.java
@@ -0,0 +1,144 @@
+package linkedlist;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+/**
+ * Created by ITHILLEL6 on 17.07.2015.
+ */
+public class LinkedList implements Collection {
+    private Element first;
+
+
+    //TODO optimize
+    @Override
+    public int size() {
+        int size = 0;
+        Element currentElement = first;
+        while(currentElement != null){
+            currentElement = currentElement.getNext();
+            size++;
+        }
+
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+
+        return first==null;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        boolean result = false;
+        Element currentElement = first;
+        while(currentElement != null){
+            if (currentElement.getData().equals(o)){
+                return true;
+            }
+
+            currentElement = currentElement.getNext();
+        }
+        return false;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public boolean add(Object o) {
+        Element toAdd = new Element(null, o);
+        Element lastElement = getLastElement();
+        if(lastElement == null){
+            first = toAdd;
+        }else{
+            lastElement.setNext(toAdd);
+        }
+        return false;
+    }
+
+    private Element getLastElement() {
+        Element lastElement = first;
+        if (lastElement == null) {
+            return lastElement;
+        }
+        while(lastElement.getNext() != null){
+            lastElement = lastElement.getNext();
+        }
+        return lastElement;
+
+    }
+
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean retainAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public Object[] toArray(Object[] a) {
+        return new Object[0];
+    }
+
+    @Override
+    public String toString (){
+        String result = "";
+        Element currentElement = first;
+        if(currentElement==null){
+            return result;
+        }
+        while(currentElement != null){
+            result += currentElement.getData() + ", ";
+            currentElement = currentElement.getNext();
+            }
+        return result;
+
+    }
+
+
+
+    public static void main(String[] args) {
+       LinkedList List1 = new LinkedList();
+        System.out.println(List1);
+        List1.add(2.5);
+        System.out.println(List1);
+        List1.add(2.5);
+        System.out.println(List1);
+        List1.add(null);
+        System.out.println(List1);
+
+    }
+}
